ca11d8c8cd62565bbd3e3f7689bc22f0c7f7d6ea
Williamf40 <63164108+necrocest@users.noreply.github.com>
2025-04-07 01:48:20 +0000

    operaciones e imprimir

---
+++ variables2.py
@@ -0,0 +1,31 @@
+first_name = "william"
+last_name = "mendoza"
+full_name = "William Mendoza"
+country="colombia"
+city="pamplona"
+age="24"
+year="2025"
+is_married="false"
+first_name, last_name, age, city = "william", "mendoza", "25", "pamplona"
+print(len(first_name))
+print(len(last_name))
+print(len(age))     #verificar la clase o tipo de variable
+print(len(city))
+print(len(country))
+print(first_name)
+print(last_name)
+print(age)   #verificar la clase o tipo de variable
+print(city)
+print(country)
+num_one = 5 
+num_two = 6
+total = num_one + num_two
+print("el total es: ", total)
+diff = num_two - num_one
+print("el total es: ", diff)
+product = num_two * num_one
+print("el total es: ", product)
+divison = num_one/num_two
+print("el total es: ", divison)
+module = num_one % num_two
+print("el total es: ", module)
