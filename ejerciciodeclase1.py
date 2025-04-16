6e39b8a4b98b286e5969675f12f54e362e6627c4
Williamf40 <63164108+necrocest@users.noreply.github.com>
2025-04-16 18:25:46 +0000

    funciones e if-elif

--- ejerciciodeclase1.py
+++ ejerciciodeclase1.py
@@ -14,16 +14,15 @@ def new_func():
         mi_promedio = (a + b + c) / 3
         print("Su promedio es: ", mi_promedio)
         return mi_promedio
-    while a < 5:
-        mensaje()
-        a = float(input("Introduzca la nota 1: "))
-        if(0.0 <= a >=5.0): 
-            print("su nota es invalida")
-        b = float(input("Introduzca la nota 2: "))
-        if(0.0 <= b >=5.0):
-            print("su nota es invalida")
-        c = float(input("Introduzca la nota 3: "))
-        if(0.0 <= c >=5.0): 
-            print("su nota es invalida")
-        operacion_promedio(a, b, c)
+    mensaje()
+    a = float(input("Introduzca la nota 1: "))
+    if(0.0 <= a >=5.0): 
+        print("su nota es invalida")
+    b = float(input("Introduzca la nota 2: "))
+    if(0.0 <= b >=5.0):
+        print("su nota es invalida")
+    c = float(input("Introduzca la nota 3: "))
+    if(0.0 <= c >=5.0): 
+        print("su nota es invalida")
+    operacion_promedio(a, b, c)
 new_func()
