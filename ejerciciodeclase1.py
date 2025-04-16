c0d456d81c4dd8053b231fda3bbf5d27893dc539
Williamf40 <63164108+necrocest@users.noreply.github.com>
2025-04-16 18:08:37 +0000

    Funciones e If-elif-else

---
+++ ejerciciodeclase1.py
@@ -0,0 +1,28 @@
+'''
+1. Un estudiante desea conocer su desempeño en una materia y 
+necesita calcular el promedio de tres calificaciones. 
+Escribe un programa que solicite tres notas al usuario y 
+calcule su promedio aritmético. Asegúrate de que los valores 
+ingresados sean números válidos y estén dentro del rango de 0 a 100.
+'''
+def new_func():
+    print("Promedio De tres Materias")
+    def mensaje():
+        print("las notas deben ser en decimal de 0.0 a 5.0")
+        print("Introduzca sus notas")
+    def  operacion_promedio(a, b, c):
+        mi_promedio = (a + b + c) / 3
+        print("Su promedio es: ", mi_promedio)
+        return mi_promedio
+    mensaje()
+    a = float(input("Introduzca la nota 1: "))
+    if(0.0 <= a >=5.0): 
+        print("su nota es invalida")
+    b = float(input("Introduzca la nota 2: "))
+    if(0.0 <= b >=5.0):
+        print("su nota es invalida")
+    c = float(input("Introduzca la nota 3: "))
+    if(0.0 <= c >=5.0): 
+        print("su nota es invalida")
+    operacion_promedio(a, b, c)
+new_func()
