8cab557bc37ccaf9ca2b9c1fd1a6002fa95d99f1
Williamf40 <63164108+necrocest@users.noreply.github.com>
2025-04-17 16:54:37 +0000

    ejerciciotemperatura

--- ejerciciotemperatura.py
+++ ejerciciotemperatura.py
@@ -3,9 +3,15 @@ Temperatura
 Para convertir grados Celsius a Fahrenheit, se utiliza la fórmula F = (C * 9/5) + 32. 
 Para convertir grados Fahrenheit a Celsius, se utiliza la fórmula C = (F - 32) * 5/9
 """
-def fahrenheit( c ): 
-    fahrenheit = (c * 9/5) + 32
-    print( "Su temperatura es: ", fahrenheit)
-    return fahrenheit
-c = float(input("ingres la temperatura en celsius: "))
-fahrenheit(c)
+def fahrenheit(f): 
+    fahren = (f * 9/5) + 32
+    print("Su temperatura en Fahrenheit es : ", fahren)
+    return fahren
+def celcius(fahren):
+    cels = (fahren-32) * 5/9
+    print("Su temperatura es: ", cels)
+    return cels
+f = float(input("ingres la temperatura en celsius: "))
+fahrenheit(f)
+celcius(fahren = (f * 9/5) + 32)
+
