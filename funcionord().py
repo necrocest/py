06545d3193f91274b5442abb89f436f24cff54bd
Williamf40 <63164108+necrocest@users.noreply.github.com>
2025-04-18 16:27:37 +0000

    acsii

--- funcionord().py
+++ funcionord().py
@@ -1,3 +1,9 @@
-caracter = str(input("ingrese el caracter del cual quiere saber el valor acsii"))
+"""
+La función ord() es especialmente útil cuando necesitas 
+trabajar con caracteres y sus valores numéricos, por ejemplo, 
+en algoritmos de cifrado, ordenación de caracteres, o simplemente 
+para entender la representación interna de los caracteres
+"""
+caracter = str(input("ingrese el caracter del cual quiere saber el valor acsii: "))
 valor_acssi = ord(caracter)
 print("el valor del caracter es: ", valor_acssi)
